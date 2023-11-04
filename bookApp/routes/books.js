209a6c301f67e0694d42140d68b277dfc6fa4d95
Shreyeska <silwalshreyeska@gmail.com>
2023-11-04 11:54:44 +0545

    Completed CRUD Operations

--- bookApp/routes/books.js
+++ bookApp/routes/books.js
@@ -14,11 +14,27 @@ router.get('/edit/:_id', function(req, res, next){
     const book = books.find((book)=>book._id === req.params._id)
     res.render('editBooks', {title: "Edit Books", book})
   })
+  router.get('/delete/:_id', function(req, res, next) {
+    const delIndex = books.findIndex((book) => book._id === req.params._id);
+    if (delIndex !== -1) {
+        books.splice(delIndex, 1);
+        console.log(`Book with _id ${req.params._id} deleted`);
+    } else { console.log(`Book with _id ${req.params._id} not found`); }
+
+    res.redirect('/');
+  });
 
   router.post('/saveEdited/:_id', function (req, res, next){
     const currIndex = books.findIndex((book) => book._id === req.params._id)
     books.splice(currIndex, 1, {...req.body, _id:req.params._id})
     res.redirect('/')
   })
+  /*
+  router.post('/saveDelete/:_id', function (req, res, next){
+    const currIndex = books.findIndex((book) => book._id === req.params._id)
+    
+    res.redirect('/')
+  })
 
+  */
 module.exports = router;
